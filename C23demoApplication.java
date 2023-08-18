fd6f039ca0133e60dd654172c6a4b6287d226ecc
Theoxylo <theoxylo@yahoo.com>
2023-08-18 05:59:12 -0700

    Added project for Kafka Streams
    with and without spring added support

---
+++ C23demoApplication.java
@@ -0,0 +1,13 @@
+package com.c23.demo.c23demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C23demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C23demoApplication.class, args);
+	}
+
+}
